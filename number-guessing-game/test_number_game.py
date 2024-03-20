f7156fb48900d85eca34fbb25963621ed98e52cb
aimei60 <104590763+aimei60@users.noreply.github.com>
2024-03-20 08:14:06 +0000

    finished testing for the quiz game. Created a testing file for my number guessing game. Then refactored the code for the game by putting them into 2 functions. This is so its easier to test

---
+++ number-guessing-game/test_number_game.py
@@ -0,0 +1,7 @@
+#tests: will use mocking and patch testing due to user and print statements
+#test 1: if top number is less than 0
+#test 2: top number isdigit
+#test 3: if random number prints out 1 number
+#test 4: user guesses return 1 work for user_guess > number:
+#test 5: user guesses return 1 work for user_guess < number:
+#test 6: user guess == numberm gueses return 1
--- quiz/test_quiz.py
+++ quiz/test_quiz.py
@@ -7,10 +7,18 @@ class TestQuizGame(unittest.TestCase):
     @patch('builtins.print')
     def test_quiz_all_correct(self, mock_print, mock_input):
         quiz()
+        #test for mock print
         mock_print.assert_any_call("Your score is:", 5, "out of 5")
         mock_print.assert_any_call("The percentage of your score is", 100.0, "%")
         #test for mock input
-
+        question_calls = [call("1. What is the deepest ocean trench? "),
+                call("2. What is the largest island in the world? "),
+                call("3. Who wrote the famous novel To Kill a Mockingbird? "),
+                call("4. What is the name of the scale used to measure spiciness of peppers? "),
+                call("5. What is the capital of Australia? ",)
+                ]
+        #check why calls not working
+        mock_input.assert_has_calls(question_calls, any_order=False)
 
 if __name__ == '__main__':
     unittest.main()
